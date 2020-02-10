7df77bb3f4a4e6249818a8ac64164395ee874d90
aaronjan <aaronjan98@gmail.com>
2020-02-10 13:34:07 -0800

    created post request to create a user

--- index.js
+++ index.js
@@ -31,5 +31,22 @@ server.get('/api/users/:id', (req, res) => {
     });
 })
 
+server.post('/api/users', (req, res) => {
+    const userInfo = req.body;
+
+    db.insert(userInfo).then(user => {
+        switch(user){
+            case user.name: case user.bio:
+                res.status(400).json({ errorMessage: "Please provide name and bio for the user." })
+                break;
+            default:
+                res.status(201).json(user)
+        }
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json({ errorMessage: "There was an error while saving the user to the database" });
+    });
+})
+
 const port = 5000;
 server.listen(port, () => console.log(`API running on port ${port}`));
