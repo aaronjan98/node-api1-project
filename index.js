5ea5264939131d81e38a35ee02333f12d8dcc6dd
aaronjan <aaronjan98@gmail.com>
2020-02-10 12:46:59 -0800

    made get request to retrieve users

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express');
 
+const db = require('./data/db.js');
+
 const server = express();
+
+server.use(express.json());
 
 server.get('/', (req, res) => {
   res.send('Hello World');
 });
+
+server.get('/api/users', (req, res) => {
+    db.find().then(users => {
+        res.status(200).json(users);
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json({ errorMessage: 'wasn\'t able to retrieve users' })
+    });
+})
 
 const port = 5000;
 server.listen(port, () => console.log(`API running on port ${port}`));
