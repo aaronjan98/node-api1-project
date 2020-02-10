3bb0fb928bcc274656e48ae575658918a6a2076d
aaronjan <aaronjan98@gmail.com>
2020-02-10 12:31:10 -0800

    installed express

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// implement your API here
+const express = require('express');
+
+const server = express();
+
+server.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+const port = 5000;
+server.listen(port, () => console.log(`API running on port ${port}`));
